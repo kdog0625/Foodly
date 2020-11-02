ca1a702bc37df9d99b4f025e8ffbb14dee5236d4
kdog0625 <kdoooog34@gmail.com>
2020-11-02 14:56:20 +0900

    ナビバー基本設定

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar bg-dark navbar-dark navbar-expand-md fixed-top">
+
+<a class="navbar-brand ml-5" href="#">Foodly</a>
+
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link mr-2" href="">ログイン</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link mr-2" href="">ゲストログイン</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link mr-5" href="">新規登録</a>
+    </li>
+  </ul>
+</nav>

811cb405fb857b2d57160b7969fc924a4a46c57d
kdog0625 <kdoooog34@gmail.com>
2020-11-02 14:31:05 +0900

    トップページファイル作成

---
+++ resources/views/tweets/index.blade.php
@@ -0,0 +1,15 @@
+@extends('app')
+
+@section('title', 'Food一覧')
+
+@section('content')
+@include('nav')
+<div class="container">
+</div>
+
+
+
+@endsection
+
+
+<div class="container" style="margin-top:50px;">

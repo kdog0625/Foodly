92ce26fafd822b5e0656d5c097e890940d082cae
kdog0625 <kdoooog34@gmail.com>
2020-11-03 00:44:10 +0900

    TweetControllerのダミーデータ削除

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Tweet;
+
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
@@ -9,20 +11,7 @@ class TweetController extends Controller
     //
     public function index()
     {
-        //ダミーデータ設定
-        // ダミーデータ
-        $tweets = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-        ];
+        $tweets = Tweet::all()->sortByDesc('created_at');
         //tweetsというキーを定義してビューファイル側で$tweetsという変数が使用できるようにする
         return view('tweets.index', ['tweets' => $tweets]);
     }

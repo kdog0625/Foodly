1801d78047818610c6be3dc5c1761ca29975d7e5
kdog0625 <kdoooog34@gmail.com>
2020-11-04 10:58:03 +0900

    トップページ用の背景画像設定

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -4,12 +4,17 @@
 
 @section('content')
 @include('nav')
+<!-- <div class="jumbotron"  style="background:url(images/bacl.jpg); background-size:cover;">
+  </div> -->
+  <div class="imgwrapper"  style="margin-top:56px;">
+   <img src="images/image01.jpg" class="img-responsive" style="width: 100%; background-size:cover">
+</div>
 <div class="container">
+<div class="row">
+      
+    </div>
 </div>
 
 
 
 @endsection
-
-
-<div class="container" style="margin-top:50px;">
